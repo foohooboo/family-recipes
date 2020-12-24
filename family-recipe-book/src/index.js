7a0dba4b3d559c91454b1b0f4c865dfa31a31feb
foohooboo <danielmarshallevans@gmail.com>
2020-12-24 10:31:10 -0700

    initial Apollo config

--- family-recipe-book/src/index.js
+++ family-recipe-book/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
